a5a55e531b1c7bed1e5bc8dfd980075a4b1cf060
VladimirHulagov <127200573+VladimirHulagov@users.noreply.github.com>
2025-08-11 16:40:37 +0300

    Создание отсутствующих файлов App.tsx и main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './shared/styles/global.css';
-import './shared/styles/colors.css';
-import './shared/styles/variables.css';
-import './shared/styles/variables-dark.css';
-import './shared/styles/antd-override.css';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
-);
+  </React.StrictMode>,
+)
